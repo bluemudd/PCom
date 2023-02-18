9918f9efcc20f4a4f74d7c40916d7b7afee12c18
bluemudd <parkjm0817@gmail.com>
2023-02-19 02:02:01 +0900

    first commit

---
+++ src/main/java/com/cos/pcom/PcomApplication.java
@@ -0,0 +1,13 @@
+package com.cos.pcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/pcom/PcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.pcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
